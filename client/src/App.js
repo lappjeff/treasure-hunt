5fd851252f6c15a4c39b2b43aa0c5d50ee6e7257
Andy Bettisworth <andy@accreu.com>
2019-10-23 06:16:45 -0500

    Spec out visualization

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,31 @@ function App() {
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Typography variant="h1" style={{fontSize: '38px', textAlign: 'center'}}>LAMDA TREASURE HUNT</Typography>
-        {/* Map */}
-        <button onClick={traverseMap}>Traverse Map</button>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container spacing={2}>
+          <Grid item xs={9}>
+            {/* Map */}
+            Map
+          </Grid>
+          <Grid item xs={3}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                {/* RoomInfo */}
+                RoomInfo
+              </Grid>
+              <Grid item xs={12}>
+                {/* PlayerInfo */}
+                PlayerInfo
+              </Grid>
+              <Grid item xs={12}>
+                {/* Controls */}
+                Controls
+                <button onClick={traverseMap}>Traverse Map</button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
